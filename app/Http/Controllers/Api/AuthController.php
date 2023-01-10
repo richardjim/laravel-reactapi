bfa53b304be5636e11cae786dadea8a5b93f3362
richardjim <richardiyevhobu@gmail.com>
2023-01-10 16:22:10 +0100

    create auth/api controller

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+// use App\Http\Requests\Api\Auth\LoginRequest;
+
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        //
+    }
+
+    public function signup(SignupRequest $request)
+    {
+        //
+    }
+
+    public function logout(Request $request)
+    {
+        //
+    }
+}
